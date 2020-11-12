714595da63e98713c9b85a84cb89da801a762f1b
tatoro60 <56406462+tatoro60@users.noreply.github.com>
2020-11-12 16:57:32 +0400

    Create Lesson1.java

---
+++ Lesson1.java
@@ -0,0 +1,24 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        byte b = 52;
+        short s = 14200;
+        int num = 52555;
+        long l = 46155555555L;
+        boolean tf = true;
+        float f = 45.87F;
+        double d = 486.744D;
+        char character = 'a';
+        String str = "Hello World";
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(l);
+        System.out.println(tf);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(character);
+        System.out.println(str);
+
+    }
+}
