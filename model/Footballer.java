0bef073d7aab59430dcb5c309369f2a750774137
Sasha <sasha.tsatryan.01@gmail.com>
2020-11-18 23:49:14 +0400

    Homework 6

--- model/Footballer.java
+++ model/Footballer.java
@@ -1,10 +1,33 @@
 package model;
 
-public class Footballer extends Sportsman {
+public class Footballer extends Sportsman implements GeneralStatistics {
     protected int transferValue;
+    protected int playedGames;
+    protected int goals;
+    protected int assists;
 
     public Footballer() {
-        transferValue = 0;
+        playedGames = 1;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        if (goals >= 0) {
+            this.goals = goals;
+        }
+    }
+
+    public int getPlayedGames() {
+        return playedGames;
+    }
+
+    public void setPlayedGames(int playedGames) {
+        if (transferValue >= 0) {
+            this.playedGames = playedGames;
+        }
     }
 
     public int getTransferValue() {
@@ -15,5 +38,15 @@ public class Footballer extends Sportsman {
         if (transferValue >= 0) {
             this.transferValue = transferValue;
         }
+    }
+
+    @Override
+    public double ratioOfGoals() {
+        return 1.0 * goals / playedGames;
+    }
+
+    @Override
+    public double assistsPerMatch() {
+        return 1.0 * assists / playedGames;
     }
 }
--- model/GoalKeeper.java
+++ model/GoalKeeper.java
@@ -1,15 +1,33 @@
 package model;
 
-public class GoalKeeper extends Footballer {
+public class GoalKeeper extends Footballer implements GoalkepperStatistics {
     protected int cleanSheets;
-    public GoalKeeper(){
+    protected int concededGoals;
+    protected int saves;
+    public GoalKeeper() {
         cleanSheets = 0;
     }
+
     public int getCleanSheets() {
         return cleanSheets;
     }
 
     public void setCleanSheets(int cleanSheets) {
         this.cleanSheets = cleanSheets;
+    }
+
+    @Override
+    public double ratioOfCleanSheets() {
+        return 1.0 * cleanSheets / playedGames;
+    }
+
+    @Override
+    public double concededGoalPerMatch() {
+        return 1.0 * concededGoals / playedGames;
+    }
+
+    @Override
+    public double savesPerMatch() {
+        return 1.0 * saves / playedGames;
     }
 }
